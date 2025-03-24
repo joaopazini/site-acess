974a954c5c506fd1769669ad2ae4a5c12ee01cfd
joaopazini <160858045+joaopazini@users.noreply.github.com>
2025-03-24 12:34:43 +0000

    João Pazini

--- site-acessivel-projeto-inicial/script.js
+++ site-acessivel-projeto-inicial/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function (){
 const aumentaFontebotao = document.getElementById('auemntar-fonte');
 const diminuiFontebotao = document.getElementById('diminuir-fonte');
 
+const alternaContraste = document.getElementById('alterna-contraste')
+
 let tamanhoAtualFonte = 1;
 
 aumentaFontebotao.addEventListener('click', function(){
@@ -14,6 +16,9 @@ diminuiFontebotao.addEventListener('click', function(){
     tamanhoAtualFonte -= 0.1;
 document.body.style.fontSize = "${tamanhoAtualFonte}rem"
 
+})
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
 })
 
 
